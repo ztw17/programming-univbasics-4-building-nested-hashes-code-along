38d3f94794f317bfd95d3889a34bd9a1bac447ae
Zach Weber <weberzt@gmail.com>
2019-12-10 19:04:33 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -26,9 +26,9 @@ def third_challenge
         :age => "51"
         },
       :heroine => {
-        :name => "Juliet"
-        :age => "15"
-        :status => "alive"      
+        :name => "Juliet",
+        :age => "15",
+        :status => "alive"       
         },
       :heroine_friends => []
    }
