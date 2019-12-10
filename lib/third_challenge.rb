16efd754adbdf90e306026f5920309c76f82bc81
Zach Weber <weberzt@gmail.com>
2019-12-10 18:00:08 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -2,25 +2,33 @@ def third_challenge
   epic_tragedy = {
    :montague => {
       :patriarch => {
-        #your key/value pairs here
+        :name => "Lord Montague",
+        :age => "53"
         },
       :matriarch => {
-        #your key/value pairs here
+        :name => "Lady Montague",
+        :age => "54"
         },
       :hero => {
-        #your key/value pairs here
+        :name => "Romeo",
+        :age => "15",
+        :status => "alive"
         },
       :hero_friends => []
    }, 
    :capulet => {
       :patriarch => {
-        #your key/value pairs here
+        :name =>
+        :age =>
         },
       :matriarch => {
-        #your key/value pairs here
+        :name =>
+        :age =>
         },
       :heroine => {
-        #your key/value pairs here
+        :name =>
+        :age =>
+        :status =>        
         },
       :heroine_friends => []
    }
