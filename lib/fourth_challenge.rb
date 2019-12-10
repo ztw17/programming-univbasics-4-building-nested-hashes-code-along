b28fa44d23333f0dc611c4d5fc6bc505faa55792
Zach Weber <weberzt@gmail.com>
2019-12-10 19:09:29 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -5,21 +5,19 @@ def fourth_challenge
       :matriarch => {name: "Lady Montague", age: "54"},
       :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => [
-        {name: "Benvolio", age: "17", attitude: "worried"}
-        {name: "Mercutio", age: "18", attutude: "hot-headed"}
+         {name: "Benvolio", age: "17", attitude: "worried"},
+         {name: "Mercutio", age: "18", attitude: "hot-headed"}
       ]
-   }, 
+   },
    :capulet => {
       :patriarch => {name: "Lord Capulet", age: "50"},
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
-        {name: "Steven", age: "30", attitude: "confused"},
-        {name: "Nurse", age: "44", attitude: "worried"}
+          {name: "Steven", age: "30", attitude: "confused"},
+          {name: "Nurse", age: "44", attitude: "worried"}
       ]
    }
-}
-
-  
+} 
 
 end
